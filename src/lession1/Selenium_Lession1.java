07f31d014f05e572c0b8e34a5c9e356804837a94
hongnt1 <hongnt1@hongnt1.officevnpay.com>
2019-11-12 21:10:18 +0700

    LESSION 1

---
+++ src/lession1/Selenium_Lession1.java
@@ -0,0 +1,27 @@
+package lession1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class Selenium_Lession1 {
+	WebDriver driver;
+	@Test
+	public void f() {
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver","E:\\Auto test learning\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://zingmp3.vn/nghe-si/Thuy-Chi");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+}
